6d308002b1a2175037648732369e565c5a223ca3
yadavj2008 <105886306+yadavj2008@users.noreply.github.com>
2024-04-19 14:03:03 +0100

    Create test_events.sql
    added manual event tests

---
+++ test_events.sql
@@ -0,0 +1,17 @@
+insert into leaderboard.accumulated_points values ('team0',0);
+insert into leaderboard.accumulated_points values ('team2',500);
+insert into leaderboard.accumulated_points values ('team3',500);
+insert into leaderboard.accumulated_points values ('team1',1000);
+insert into leaderboard.accumulated_points values ('team2',1500);
+insert into leaderboard.accumulated_points values ('team3',2500);
+
+insert into leaderboard.accumulated_points values ('team4',2500);
+insert into leaderboard.accumulated_points values ('team5',2500);
+insert into leaderboard.accumulated_points values ('team6',2500);
+insert into leaderboard.accumulated_points values ('team7',2500);
+insert into leaderboard.accumulated_points values ('team8',2500);
+insert into leaderboard.accumulated_points values ('team9',2500);
+insert into leaderboard.accumulated_points values ('team10',2500);
+
+
+truncate table leaderboard.accumulated_points
